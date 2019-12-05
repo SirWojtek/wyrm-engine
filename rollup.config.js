d428182bfa48da6b1173bc5e78b737dd4853a329
Mateusz Okulewicz <mat@citrusad.com>
2019-12-05 17:32:32 +0100

    rollup is working

--- rollup.config.js
+++ rollup.config.js
@@ -1,123 +1,43 @@
-import { resolve } from "path";
-import sourceMaps from "rollup-plugin-sourcemaps";
-import nodeResolve from "rollup-plugin-node-resolve";
-import json from "@rollup/plugin-json";
+import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
-import replace from "@rollup/plugin-replace";
-import { terser } from "rollup-plugin-terser";
-import { getIfUtils, removeEmpty } from "webpack-config-utils";
+import sourceMaps from "rollup-plugin-sourcemaps";
+import camelCase from "lodash.camelcase";
+import typescript from "rollup-plugin-typescript2";
+import json from "rollup-plugin-json";
 
-import pkg from "./package.json";
-const {
-  pascalCase,
-  normalizePackageName,
-  getOutputFileName
-} = require("./helpers");
+const pkg = require("./package.json");
 
-/**
- * @typedef {import('./types').RollupConfig} Config
- */
-/**
- * @typedef {import('./types').RollupPlugin} Plugin
- */
+const libraryName = "wyrm-engine";
 
-const env = process.env.NODE_ENV || "development";
-const { ifProduction } = getIfUtils(env);
-
-const LIB_NAME = pascalCase(normalizePackageName(pkg.name));
-const ROOT = resolve(__dirname, "..");
-const DIST = resolve(ROOT, "dist");
-
-/**
- * Object literals are open-ended for js checking, so we need to be explicit
- * @type {{entry:{esm5: string, esm2015: string},bundles:string}}
- */
-const PATHS = {
-  entry: {
-    esm5: resolve(DIST, "esm5"),
-    esm2015: resolve(DIST, "esm2015")
-  },
-  bundles: resolve(DIST, "bundles")
-};
-
-/**
- * @type {string[]}
- */
-const external = Object.keys(pkg.peerDependencies) || [];
-
-/**
- *  @type {Plugin[]}
- */
-const plugins = /** @type {Plugin[]} */ ([
-  // Allow json resolution
-  json(),
-
-  // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-  commonjs(),
-
-  // Allow node_modules resolution, so you can use 'external' to control
-  // which external modules to include in the bundle
-  // https://github.com/rollup/rollup-plugin-node-resolve#usage
-  nodeResolve(),
-
-  // Resolve source maps to the original source
-  sourceMaps(),
-
-  // properly set process.env.NODE_ENV within `./environment.ts`
-  replace({
-    exclude: "node_modules/**",
-    "process.env.NODE_ENV": JSON.stringify(env)
-  })
-]);
-
-/**
- * @type {Config}
- */
-const CommonConfig = {
-  input: {},
-  output: {},
-  inlineDynamicImports: true,
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external
-};
-
-/**
- * @type {Config}
- */
-const UMDconfig = {
-  ...CommonConfig,
-  input: resolve(PATHS.entry.esm5, "index.js"),
-  output: {
-    file: getOutputFileName(
-      resolve(PATHS.bundles, "index.umd.js"),
-      ifProduction()
-    ),
-    format: "umd",
-    name: LIB_NAME,
-    sourcemap: true
-  },
-  plugins: removeEmpty(/** @type {Plugin[]} */ ([...plugins]))
-};
-
-/**
- * @type {Config}
- */
-const FESMconfig = {
-  ...CommonConfig,
-  input: resolve(PATHS.entry.esm2015, "index.js"),
+export default {
+  input: `src/${libraryName}.ts`,
   output: [
     {
-      file: getOutputFileName(
-        resolve(PATHS.bundles, "index.esm.js"),
-        ifProduction()
-      ),
-      format: "es",
+      file: pkg.main,
+      name: camelCase(libraryName),
+      format: "umd",
       sourcemap: true
-    }
+    },
+    { file: pkg.module, format: "es", sourcemap: true }
   ],
-  plugins: removeEmpty(
-    /** @type {Plugin[]} */ ([...plugins, ifProduction(terser())])
-  )
-};
+  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+  external: [],
+  watch: {
+    include: "src/**"
+  },
+  plugins: [
+    // Allow json resolution
+    json(),
+    // Compile TypeScript files
+    typescript({ useTsconfigDeclarationDir: true }),
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs(),
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve(),
 
-export default [UMDconfig, FESMconfig];
+    // Resolve source maps to the original source
+    sourceMaps()
+  ]
+};
