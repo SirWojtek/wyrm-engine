76aab4c96146f42f399c7061a420c4f1d2726662
Mateusz Okulewicz <mat@citrusad.com>
2019-12-19 14:45:29 +0100

    refactored tests

--- src/test/Encounter.spec.ts
+++ src/test/Encounter.spec.ts
@@ -6,6 +6,8 @@ import {
 import { Engine } from '../Engine';
 import {
   createEngine,
+  Encounter,
+  IBaseCharacter,
   IDeathEncounterLogEntry,
   IWinEncounterLogEntry,
   LogEntryTypeEnum,
@@ -21,102 +23,92 @@ describe('Encounter', () => {
     characterCreator = engine.getCharacterCreator();
   });
 
-  test('do nothing if actions are not set', () => {
-    const kyle = characterCreator.createCharacter({
-      name: 'Kyle',
-      level: 10,
-      type: CharacterTypeEnum.Strong,
-      subtype: CharacterSubtypeEnum.Balanced,
-    });
-    const jenny = characterCreator.createCharacter({
-      name: 'Jenny',
-      level: 10,
-      type: CharacterTypeEnum.Strong,
-      subtype: CharacterSubtypeEnum.Balanced,
-    });
+  describe('actions', () => {
+    let kyle: IBaseCharacter;
+    let jenny: IBaseCharacter;
+    let encounter: Encounter;
 
-    const encounter = engine.createEncounter([kyle], [jenny]);
-
-    encounter.tick();
-    expect(encounter.getEncounterLog().length).toEqual(1);
-
-    encounter.tick();
-    expect(encounter.getEncounterLog().length).toEqual(2);
-  });
-
-  test('character with action set won', () => {
-    const kyle = characterCreator.createCharacter({
-      name: 'Kyle',
-      level: 10,
-      type: CharacterTypeEnum.Strong,
-      subtype: CharacterSubtypeEnum.Balanced,
-    });
-    const jenny = characterCreator.createCharacter({
-      name: 'Jenny',
-      level: 10,
-      type: CharacterTypeEnum.Strong,
-      subtype: CharacterSubtypeEnum.Balanced,
+    beforeEach(() => {
+      kyle = characterCreator.createCharacter({
+        name: 'Kyle',
+        level: 10,
+        type: CharacterTypeEnum.Strong,
+        subtype: CharacterSubtypeEnum.Balanced,
+      });
+      jenny = characterCreator.createCharacter({
+        name: 'Jenny',
+        level: 10,
+        type: CharacterTypeEnum.Strong,
+        subtype: CharacterSubtypeEnum.Balanced,
+      });
+      encounter = engine.createEncounter([kyle], [jenny]);
     });
 
-    const encounter = engine.createEncounter([kyle], [jenny]);
-    encounter.addAction(kyle.id, kyle.actions[0]);
+    test('do nothing if actions are not set', () => {
+      encounter.tick();
+      expect(encounter.getEncounterLog().length).toEqual(1);
 
-    while (encounter.tick()) {
+      encounter.tick();
+      expect(encounter.getEncounterLog().length).toEqual(2);
+    });
+
+    test('character with action set won', () => {
       encounter.addAction(kyle.id, kyle.actions[0]);
-    }
 
-    const winMessage: IWinEncounterLogEntry = encounter
-      .getEncounterLog()
-      .find(l => l.entryType === LogEntryTypeEnum.Win) as IWinEncounterLogEntry;
-    const deathMessage: IDeathEncounterLogEntry = encounter
-      .getEncounterLog()
-      .find(
-        l => l.entryType === LogEntryTypeEnum.Death,
-      ) as IDeathEncounterLogEntry;
+      while (encounter.tick()) {
+        encounter.addAction(kyle.id, kyle.actions[0]);
+      }
 
-    expect(winMessage).toBeDefined();
-    expect(winMessage.wictoryTeam).toEqual(TeamEnum.teamA);
+      const winMessage: IWinEncounterLogEntry = encounter
+        .getEncounterLog()
+        .find(
+          l => l.entryType === LogEntryTypeEnum.Win,
+        ) as IWinEncounterLogEntry;
+      const deathMessage: IDeathEncounterLogEntry = encounter
+        .getEncounterLog()
+        .find(
+          l => l.entryType === LogEntryTypeEnum.Death,
+        ) as IDeathEncounterLogEntry;
 
-    expect(deathMessage).toBeDefined();
-    expect(deathMessage.killed.id).toEqual(jenny.id);
-  });
+      expect(winMessage).toBeDefined();
+      expect(winMessage.wictoryTeam).toEqual(TeamEnum.teamA);
 
-  test('AI character with action set won', () => {
-    const kyle = characterCreator.createCharacter({
-      name: 'Kyle',
-      level: 10,
-      type: CharacterTypeEnum.Strong,
-      subtype: CharacterSubtypeEnum.Balanced,
-      overrideCharacter: {
-        controllerCallback: actions => actions[0],
-      },
-    });
-    const jenny = characterCreator.createCharacter({
-      name: 'Jenny',
-      level: 10,
-      type: CharacterTypeEnum.Strong,
-      subtype: CharacterSubtypeEnum.Balanced,
+      expect(deathMessage).toBeDefined();
+      expect(deathMessage.killed.id).toEqual(jenny.id);
     });
 
-    const encounter = engine.createEncounter([kyle], [jenny]);
+    test('AI character with action set won', () => {
+      kyle = characterCreator.createCharacter({
+        name: 'Kyle',
+        level: 10,
+        type: CharacterTypeEnum.Strong,
+        subtype: CharacterSubtypeEnum.Balanced,
+        overrideCharacter: {
+          controllerCallback: actions => actions[0],
+        },
+      });
+      encounter = engine.createEncounter([kyle], [jenny]);
 
-    while (encounter.tick()) {
-      // noop
-    }
+      while (encounter.tick()) {
+        // noop
+      }
 
-    const winMessage: IWinEncounterLogEntry = encounter
-      .getEncounterLog()
-      .find(l => l.entryType === LogEntryTypeEnum.Win) as IWinEncounterLogEntry;
-    const deathMessage: IDeathEncounterLogEntry = encounter
-      .getEncounterLog()
-      .find(
-        l => l.entryType === LogEntryTypeEnum.Death,
-      ) as IDeathEncounterLogEntry;
+      const winMessage: IWinEncounterLogEntry = encounter
+        .getEncounterLog()
+        .find(
+          l => l.entryType === LogEntryTypeEnum.Win,
+        ) as IWinEncounterLogEntry;
+      const deathMessage: IDeathEncounterLogEntry = encounter
+        .getEncounterLog()
+        .find(
+          l => l.entryType === LogEntryTypeEnum.Death,
+        ) as IDeathEncounterLogEntry;
 
-    expect(winMessage).toBeDefined();
-    expect(winMessage.wictoryTeam).toEqual(TeamEnum.teamA);
+      expect(winMessage).toBeDefined();
+      expect(winMessage.wictoryTeam).toEqual(TeamEnum.teamA);
 
-    expect(deathMessage).toBeDefined();
-    expect(deathMessage.killed.id).toEqual(jenny.id);
+      expect(deathMessage).toBeDefined();
+      expect(deathMessage.killed.id).toEqual(jenny.id);
+    });
   });
 });
